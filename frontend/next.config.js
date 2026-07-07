553a669b1b16b372d6c0ec32a6f3ca9028e626d7
Karam Singh <bhat84617@gmail.com>
2026-07-07 20:46:09 +0530

    Remove async rewrites and update config options

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      { source: '/api/:path*', destination: 'http://localhost:8000/api/:path*' },
-    ]
-  },
+  output: 'export',
+  images: { unoptimized: true },
 }
 module.exports = nextConfig
